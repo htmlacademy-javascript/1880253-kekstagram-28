4b6c31f51dc12d35e61396af4122e8e150d840ba
Maksim Opriatin <maksim.opryatin@yandex.ru>
2023-02-22 10:47:12 +0300

    fix: variables names in functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,34 @@
 // Функция проверки длины строки
-const checkLength = (string, characters = 18) => string.length >= characters;
-checkLength('проверяемая строк', 18);
+const isValidLength = (initString, characters = 18) => initString.length >= characters;
+isValidLength('проверяемая строк', 18);
 
 // Функция проверки на палиндром
-function checkPalindrom(string) {
-  string = string.toLowerCase().replaceAll(' ', '');
-  const result = string === string.split('').reverse().join('');
-  return result;
+function isPalindrom(initString) {
+  const tempString = initString.toLowerCase().replaceAll(' ', '');
+  return tempString === tempString.split('').reverse().join('');
 }
 
-checkPalindrom('Лёша на полке клопа нашёл ');
+isPalindrom('Лёша на полке клопа нашёл ');
 
 // Функция, извлекающая цифры из строки
-function getNumber(string) {
-  if (typeof string === 'number') {
-    string = String(string);
-    string = string.replaceAll('-', '').replaceAll('.', '');
-    return Number(string);
+function genNumber(initString) {
+  const convertToString = String(initString);
+  let result = '';
+  const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  for (let i = 0; i <= convertToString.length; i++) {
+    for (let j = 0; j < numbers.length; j++) {
+      if (convertToString[i] === numbers[j]) {
+        result += convertToString[i];
+      }
+    }
   }
-  return parseInt(string.replace(/[^\d]/g, ''), 10);
+  if (result === '') {
+    return NaN;
+  }
+  return Number(result);
 }
 
-getNumber('2000 string 2033');
+genNumber(1.5);
 
 // Функция с тремя параметрами
 function modifyString(string, reqLength, symbols) {
@@ -29,26 +36,26 @@ function modifyString(string, reqLength, symbols) {
   let result;
   if (string.length >= reqLength) {
     return string;
-  } else {
-    result = string;
-    if (symbols.length >= reqLength) {
-      result = symbols.slice(0, ((symbols.length + 1 - reqLength) * -1)) + string;
-      return result;
-    }
-    result = symbols + string;
-    if (result.length < reqLength && result.length + 1 === reqLength) {
+  }
+  result = string;
+  if (symbols.length >= reqLength) {
+    result = symbols.slice(0, ((symbols.length + 1 - reqLength) * -1)) + string;
+    return result;
+  }
+  result = symbols + string;
+  if (result.length < reqLength && result.length + 1 === reqLength) {
+    result = symbols.slice(0, 1) + result;
+    return result;
+  }
+  result = string;
+  for (let i = string.length; i < reqLength; i++) {
+    if (string.length < reqLength) {
+      result = symbols + result;
+    } else if (result.length < reqLength && result.length + 1 === reqLength) {
       result = symbols.slice(0, 1) + result;
-      return result;
-    }
-    result = string;
-    for (let i = string.length; i < reqLength; i++) {
-      if (string.length < reqLength) {
-        result = symbols + result;
-      } else if (result.length < reqLength && result.length + 1 === reqLength) {
-        result = symbols.slice(0, 1) + result;
-      }
     }
   }
+
   return result;
 }
 modifyString('qwerty', 4, '0');
