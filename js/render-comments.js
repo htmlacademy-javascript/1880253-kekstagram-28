b15fe8ac9ef9b89d4b6a359a5054ff4bc203b7a1
Maksim <maksim.opryatin@yandex.ru>
2023-03-26 22:37:31 +0300

    feat: add open-popup.js and render-comments.js

---
+++ js/render-comments.js
@@ -0,0 +1,30 @@
+import { bigPicture } from './open-popup.js';
+
+const commentsContainer = document.querySelector('.social__comments');
+
+const createCommentInPopup = (element) => {
+  const newComment = `
+  <li class="social__comment">
+    <img class="social__picture" src="${element.avatar}" alt="${element.name}" width="35" height="35">
+    <p class="social__text">${element.message}</p>
+  </li>
+  `;
+  return newComment;
+};
+
+const createCommentsInPopup = (array) => {
+  let commentString = '';
+  for (const element of array) {
+    commentString += createCommentInPopup(element);
+  }
+  commentsContainer.innerHTML = commentString;
+};
+
+const renderBigPictureInfo = (object) => {
+  bigPicture.querySelector('.big-picture__img>img').src = object.url;
+  bigPicture.querySelector('.likes-count').textContent = object.likes;
+  bigPicture.querySelector('.comments-count').textContent = object.comments.length;
+  createCommentsInPopup(object.comments);
+};
+
+export { renderBigPictureInfo };
--- js/render-pictures.js
+++ js/render-pictures.js
@@ -1,10 +1,8 @@
 import { createPhotoArray } from './data.js';
+import { openPicturePopup, closePicturePopup } from './open-popup.js';
+import { renderBigPictureInfo } from './render-comments.js';
 
 const allPictures = document.querySelector('.pictures');
-if (allPictures) {
-  allPictures.querySelector('.pictures__title').classList.remove('visually-hidden');
-}
-
 const pictureTemplate = document.querySelector('#picture').content.cloneNode(true);
 const pictureFragment = document.createDocumentFragment();
 
@@ -18,10 +16,19 @@ const placePictures = function (photos) {
     newPictureTemplate.querySelector('.picture__likes').textContent = likes;
     newPictureTemplate.querySelector('.picture__comments').textContent = comments.length;
     pictureFragment.append(newPictureTemplate);
-
   });
   if (allPictures) {
     allPictures.append(pictureFragment);
+    const picture = document.querySelectorAll('.picture');
+    for (let i = 0; i < picture.length; i++) {
+      const eachPicture = picture[i];
+      eachPicture.addEventListener('click', (evt) => {
+        evt.preventDefault();
+        openPicturePopup();
+        renderBigPictureInfo(photos[i]);
+      });
+    }
+    closePicturePopup();
   }
 };
 
