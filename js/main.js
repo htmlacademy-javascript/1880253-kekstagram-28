0aff4c4435eb549488bf76af2b525d1da1b82470
Maksim <maksim.opryatin@yandex.ru>
2023-03-29 19:25:43 +0300

    feat: add comments load check at comments-check.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 import { placePictures, photoArray } from './render-pictures.js';
-import { hideCounters } from './open-popup.js';
 
 document.querySelector('.pictures__title').classList.remove('visually-hidden');
 
 placePictures(photoArray);
-hideCounters();
+
