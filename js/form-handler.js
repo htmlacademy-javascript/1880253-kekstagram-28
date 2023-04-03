d40f7ac48c71ff4090ea2f63c29f459b1df78957
Maksim <maksim.opryatin@yandex.ru>
2023-04-03 03:02:00 +0300

    feat: add form validation at validation.js

---
+++ js/form-handler.js
@@ -0,0 +1,43 @@
+import { isEscapeKey } from './util.js';
+// import './validation.js';
+import { hashtagInput, commentInput } from './validation.js';
+
+const imageToChange = document.querySelector('.img-upload__preview img');
+const fileHandler = new FileReader();
+const uploadImageInput = document.querySelector('#upload-file');
+const overlay = document.querySelector('.img-upload__overlay');
+const closeEditorButton = document.querySelector('.img-upload__cancel');
+
+const fieldInFocus = () => document.activeElement === hashtagInput || document.activeElement === commentInput;
+
+const onOverlayKeydown = (evt) => {
+  if (isEscapeKey(evt) && !fieldInFocus()) {
+    evt.preventDefault();
+    overlay.classList.add('hidden');
+    uploadImageInput.value = '';
+    document.querySelector('body').classList.remove('modal-open');
+  }
+};
+
+
+const closeEditor = function (item) {
+  item.addEventListener('click', () => {
+    overlay.classList.add('hidden');
+    document.querySelector('body').classList.remove('modal-open');
+    document.removeEventListener('keydown', onOverlayKeydown);
+    document.querySelector('.img-upload__overlay').classList.add('hidden');
+    uploadImageInput.value = '';
+  });
+};
+
+
+uploadImageInput.addEventListener('change', () => {
+  overlay.classList.remove('hidden');
+  fileHandler.onload = function () {
+    document.querySelector('body').classList.add('modal-open');
+    document.addEventListener('keydown', onOverlayKeydown);
+    imageToChange.src = fileHandler.result;
+  };
+  fileHandler.readAsDataURL(uploadImageInput.files[0]);
+  closeEditor(closeEditorButton);
+});
--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import { placePictures, photoArray } from './render-pictures.js';
+import './form-handler.js';
 
 document.querySelector('.pictures__title').classList.remove('visually-hidden');
 
 placePictures(photoArray);
-
